fea716f95bb63967e6fc38eea20096d96e395d66
Vadim Peretokin <vperetokin@gmail.com>
2016-04-11 20:11:29 +1000

    Starting on adding JSON to XML tests
    This'll be a bit of work, since JSON loses the order of elements, and that matters in XML.

--- spec/patient_example_spec.lua
+++ spec/patient_example_spec.lua
@@ -17,10 +17,11 @@
 ]]
 
 local in_fhir_json = require("fhir-formats").to_json
+local in_fhir_xml = require("fhir-formats").to_xml
 local cjson = require("cjson")
 local xml = require("xml")
 
-describe("fhir-formats", function()
+describe("xml to json", function()
     local positive_example, negative_example, patient_example,
     positive_example_data, patient_example_data
 
@@ -49,5 +50,36 @@ describe("fhir-formats", function()
         local positive_example_div = xml.load(positive_example.text.div)
         local patient_example_div = xml.load(patient_example.text.div)
         assert.same(positive_example_div, patient_example_div)
+      end)
+  end)
+
+describe("json to xml", function()
+    local positive_example, negative_example, patient_example,
+    positive_example_data, patient_example_data
+
+    setup(function()
+        io.input("spec/patient-example.xml")
+        positive_example_data = io.read("*a")
+        patient_example_data = in_fhir_xml("spec/patient-example-good.json", {file = true})
+
+        -- for same div data test
+        assert:set_parameter("TableFormatLevel", -1)
+      end)
+
+    before_each(function()
+        positive_example = xml.load(positive_example_data)
+        patient_example = xml.load(patient_example_data)
+      end)
+
+    it("should have the same non-div data", function()
+        -- cut out the div's, since the whitespace doesn't matter as much in xml
+
+--        positive_example.text.div = nil
+--        patient_example.text.div = nil
+        assert.same(positive_example, patient_example)
+      end)
+
+    it("should have xml-comparable div data", function()
+        assert.same(positive_example.text.div, patient_example.text.div)
       end)
   end)
