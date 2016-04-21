e2fcecc10a19ae08226aff3a0bcf0810efbb98b9
Vadim Peretokin <vperetokin@gmail.com>
2016-04-22 08:13:25 +1000

    Try using an empty table to represent a null value
    math.huge * 0 doesn't seem to work, causes an issue with null's in JSON.

--- src/fhirformats.lua
+++ src/fhirformats.lua
@@ -52,7 +52,7 @@ if cjson then
   null_value = cjson.null
   json_decode, json_encode = cjson.decode, cjson.encode
 elseif lunajson then
-  null_value = math.huge * 0
+  null_value = {}
   json_decode = function(data)
     return lunajson.decode(data, nil, null_value)
   end
