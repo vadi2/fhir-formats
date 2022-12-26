e0614194095017081073cfacb6b26fe0f0ca5d90
Vadim Peretokin <vperetokin@gmail.com>
2022-12-26 13:22:03 +0100

    Add devcontainer configuration (#9)

--- src/fhir-data/download-fhir.sh
+++ src/fhir-data/download-fhir.sh
@@ -1,6 +1,6 @@
 # FHIR Formats
 
-# Copyright (C) 2016-2017 Vadim Peretokin
+# Copyright (C) 2016-2017, 2022 Vadim Peretokin
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
