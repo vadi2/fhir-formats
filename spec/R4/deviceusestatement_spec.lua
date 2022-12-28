523e1573c0798ae9c4c584f9d20d26a3ec7ab4e4
Vadim Peretokin <vperetokin@hey.com>
2022-12-28 20:18:48 +0100

    Add R4 deviceusestatement tests

---
+++ spec/R4/deviceusestatement_spec.lua
@@ -0,0 +1,77 @@
+--[[
+  FHIR Formats
+
+  Copyright (C) 2022 Vadim Peretokin
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+]]
+
+package.path = "src/?.lua;"..package.path
+local in_fhir_json = require("fhirformats").to_json
+local in_fhir_xml = require("fhirformats").to_xml
+local cjson = require("cjson")
+local xml = require("xml")
+local inspect = require("inspect")
+
+describe("xml to json", function()
+    local positive_example, negative_example, deviceusestatement_example,
+    positive_example_data, deviceusestatement_example_data
+
+    setup(function()
+        io.input("spec/R4/deviceusestatement-example.json")
+        positive_example_data = io.read("*a")
+        deviceusestatement_example_data = in_fhir_json("spec/R4/deviceusestatement-example.xml", {file = true}, "R4")
+
+        -- for same div data test
+        assert:set_parameter("TableFormatLevel", -1)
+      end)
+
+    before_each(function()
+        positive_example = cjson.decode(positive_example_data)
+        deviceusestatement_example = cjson.decode(deviceusestatement_example_data)
+      end)
+
+    it("should have the same non-div data", function()
+        -- cut out the div's, since the whitespace doesn't matter as much in xml
+        positive_example.text.div = nil
+        deviceusestatement_example.text.div = nil
+        assert.same(positive_example, deviceusestatement_example)
+      end)
+
+    it("should have xml-comparable div data", function()
+        local positive_example_div = xml.load(positive_example.text.div)
+        local deviceusestatement_example_div = xml.load(deviceusestatement_example.text.div)
+        --print(inspect(positive_example_div))
+        --print(inspect(deviceusestatement_example_div))
+        assert.same(positive_example_div, deviceusestatement_example_div)
+      end)
+  end)
+
+describe("json to xml", function()
+    local positive_example, negative_example, deviceusestatement_example,
+    positive_example_data, deviceusestatement_example_data
+
+    setup(function()
+        io.input("spec/R4/deviceusestatement-example.xml")
+        positive_example_data = io.read("*a")
+        deviceusestatement_example_data = in_fhir_xml("spec/R4/deviceusestatement-example.json", {file = true}, "R4")
+
+        -- for same div data test
+        assert:set_parameter("TableFormatLevel", -1)
+      end)
+
+    it("should have the same data", function()
+        -- convert it down to JSON since order of elements doesn't matter in JSON, while it does in XML
+        assert.same(cjson.decode(in_fhir_json(positive_example_data, nil, "R4")), cjson.decode(in_fhir_json(deviceusestatement_example_data, nil, "R4")))
+      end)
+  end)
